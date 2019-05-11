efa162cc25c02b3ab9e9498c63c5b95820896056
Diana Loika <Diana_Loika@epam.com>
2019-05-11 14:39:18 +0300

    feat: add student page component

---
+++ src/app/components/student-table/student-table.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Student } from '../../common/entities/student';
+
+@Component({
+  selector: 'app-student-table',
+  templateUrl: './student-table.component.html',
+  styleUrls: ['./student-table.component.scss']
+})
+export class StudentTableComponent implements OnInit {
+
+  @Input() students: Student[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
