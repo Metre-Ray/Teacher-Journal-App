41751f8435860da61ec5a47c41bc475fcb7e982a
Diana Loika <Diana_Loika@epam.com>
2019-05-15 16:04:22 +0300

    feat:  highlight lines of student table on click using Renderer2

--- src/app/components/students/student-table/student-table.component.ts
+++ src/app/components/students/student-table/student-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2 } from '@angular/core';
 import { Student } from '../../../common/entities/student';
 
 @Component({
@@ -10,9 +10,16 @@ export class StudentTableComponent implements OnInit {
 
   @Input() students: Student[];
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  onRowClick(event) {
+    const target = event.currentTarget;
+    if (target.classList.contains('selected')) {
+      this.renderer.removeClass(target, 'selected');
+    } else { this.renderer.addClass(target, 'selected'); }
   }
 
 }
