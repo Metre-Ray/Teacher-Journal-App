312e40f616d010d9c8376487e6b3605bcb5acf65
Diana Loika <Diana_Loika@epam.com>
2019-05-16 12:30:37 +0300

    feat: add catchError function to http request

--- src/app/common/services/request-data-from-server.service.ts
+++ src/app/common/services/request-data-from-server.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,11 @@ export class RequestDataFromServerService {
   constructor(private http: HttpClient) { }
 
   getMockData(): Observable<any> {
-    return this.http.get('./assets/data.json');
+    return this.http.get('./assets/data.json').pipe(
+      catchError(err => {
+        console.log('Erorr in http request: ', err);
+        return of({});
+      })
+    );
   }
 }
