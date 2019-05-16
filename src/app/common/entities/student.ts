21b3c13266b947b3d887697ae4f8cb3443cd53a1
Diana Loika <Diana_Loika@epam.com>
2019-05-16 14:41:43 +0300

    feat: make student and subject classes instead of interfaces

--- src/app/common/entities/student.ts
+++ src/app/common/entities/student.ts
@@ -1,4 +1,6 @@
-export interface Student {
+import { genId } from '../helpers/generateId';
+
+export class Student {
   Id: string;
   Index: number;
   Name: string;
@@ -6,4 +8,14 @@ export interface Student {
   Address: string;
   Description: string;
   Marks: object;
+
+  constructor(Name: string, Surname: string,  Address: string = '', Description: string = '', Marks = {}, Index: number = 5) {
+    this.Id =  genId();
+    this.Index = Index;
+    this.Name = Name;
+    this['Last name'] = Surname;
+    this.Address = Address;
+    this.Description = Description;
+    this.Marks = Marks;
+  }
 }
--- src/app/common/entities/subject.ts
+++ src/app/common/entities/subject.ts
@@ -1,9 +1,28 @@
-export interface Subject {
+import { genId } from '../helpers/generateId';
+
+
+export class Subject {
   Id: string;
   Name: string;
-  'Teacher Name': string;
-  'Teacher Surname': string;
-  Cabinet: number;
+  TeacherName: string;
+  TeacherSurname: string;
+  Room: number;
   Description: string;
   Dates: string[];
+
+  constructor(Name: string, Teacher: string,  Room: number, Description: string = '', Dates = []) {
+    this.Id =  genId();
+    const temp = Teacher.trim().split(' ');
+    if (temp.length === 1) {
+      this.TeacherSurname = temp[0];
+      this.TeacherName = '';
+    } else {
+      this.TeacherName = temp[0];
+      this.TeacherSurname = temp[1];
+    }
+    this.Name = Name;
+    this.Room = Room;
+    this.Description = Description;
+    this.Dates = Dates;
+  }
 }
