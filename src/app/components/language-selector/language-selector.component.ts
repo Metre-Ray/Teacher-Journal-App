09c83a6319aacee47068b87da2d2477381d5b4ca
Diana Loika <Diana_Loika@epam.com>
2019-05-21 13:35:32 +0300

    feat: create dropdown for language selection and add on page

---
+++ src/app/components/language-selector/language-selector.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrls: ['./language-selector.component.scss']
+})
+export class LanguageSelectorComponent implements OnInit {
+
+  languages = [['en', 'English'], ['ru', 'Русский']];
+  flag = false;
+  label: string;
+
+  constructor(private translate: TranslateService) { }
+
+  ngOnInit() {
+    this.label = this.searchLabel(this.translate.currentLang);
+  }
+
+  showMenu() {
+    this.flag = !this.flag;
+  }
+
+  onItemClick(event, lang) {
+    event.preventDefault();
+    this.translate.use(lang);
+    this.label = this.searchLabel(this.translate.currentLang);
+  }
+
+  searchLabel(searchLang) {
+    return this.languages.find((el) => el[0] === searchLang)[1];
+  }
+
+  @HostListener('mouseleave') omMouseOver() {
+    if (this.flag) { this.flag = !this.flag; }
+  }
+}
