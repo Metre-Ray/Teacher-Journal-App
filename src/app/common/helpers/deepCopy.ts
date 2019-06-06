90122a24d5bf58d08eeb8d2bc8d754cd1572a63d
Diana Loika <Diana_Loika@epam.com>
2019-06-06 13:44:21 +0300

    refactor: change spaces and new lines

--- src/app/common/helpers/deepCopy.ts
+++ src/app/common/helpers/deepCopy.ts
@@ -1,7 +1,7 @@
 export function cloneObject(obj: object) {
   const clone = Array.isArray(obj) ? [] : {};
   for (const i in obj) {
-    if(obj[i] !== null &&  typeof(obj[i]) === 'object') {
+    if (obj[i] !== null && typeof(obj[i]) === 'object') {
       clone[i] = cloneObject(obj[i]);
     } else {
       clone[i] = obj[i];
